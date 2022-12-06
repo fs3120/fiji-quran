ab51e9504b0f257d84cd7ea8b42fea967f58a077
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-06 22:31:14 +0700

    navbar & navigate progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./router";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
