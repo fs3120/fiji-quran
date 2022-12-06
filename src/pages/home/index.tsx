9cc1a41d069cc67f81e8a32cf42b82e937e197e2
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-06 22:01:26 +0700

    home & about page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import MainCard from "../../components/MainCard";
+import PageWrapper from "../../components/PageWrapper";
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return (
+    <PageWrapper title="Homepage">
+      <MainCard>
+        <Typography variant="h4">Homepage</Typography>
+      </MainCard>
+    </PageWrapper>
+  );
+}
