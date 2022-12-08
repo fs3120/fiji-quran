4ab49e84656a3780b3147b69fa96b4162d256bfd
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-08 10:28:41 +0700

    remove mt from pagewrapper

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -5,14 +5,12 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 interface PageWrapperProps {
   title: string;
   gap?: number;
-  noGap?: boolean;
   children: ReactNode;
 }
 
 export default function PageWrapper({
   title,
   gap,
-  noGap,
   children,
 }: PageWrapperProps) {
   return (
@@ -21,7 +19,7 @@ export default function PageWrapper({
         <Helmet>
           <title>{title}</title>
         </Helmet>
-        <Box display="flex" flexDirection="column" gap={gap || 0} mt={noGap ? 0 : 1}>
+        <Box display="flex" flexDirection="column" gap={gap || 0}>
           {children}
         </Box>
       </HelmetProvider>
