cb1e2137a9b3a5fc130016ab1f5ded17dc4d7602
Fyn-x <fajri.itclub@gmail.com>
2022-12-12 11:54:57 +0700

    add new components for sidebar

---
+++ src/pages/juz/index.tsx
@@ -0,0 +1,13 @@
+import MainCard from "../../components/MainCard";
+import PageWrapper from "../../components/PageWrapper";
+import Typography from "@mui/material/Typography";
+
+export default function JuzPage() {
+  return (
+    <PageWrapper title="Juz Page">
+      <MainCard>
+        <Typography variant="h4">Ini halaman juz</Typography>
+      </MainCard>
+    </PageWrapper>
+  );
+}
---
+++ src/pages/surah/index.tsx
@@ -0,0 +1,13 @@
+import MainCard from "../../components/MainCard";
+import PageWrapper from "../../components/PageWrapper";
+import Typography from "@mui/material/Typography";
+
+export default function SurahPage() {
+  return (
+    <PageWrapper title="Surah Page">
+      <MainCard>
+        <Typography variant="h4">Ini Halaman Surah</Typography>
+      </MainCard>
+    </PageWrapper>
+  );
+}
