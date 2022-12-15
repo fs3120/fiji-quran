ffad9668cb499a70323ea49f5c28580cc79e1feb
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-15 12:27:02 +0700

    move title of each component to its component

--- src/pages/about/TeamDetail.tsx
+++ src/pages/about/TeamDetail.tsx
@@ -1,26 +1,30 @@
 import MainCard from "../../components/MainCard";
 import TeamDetailItem from "./TeamDetailItem";
+import TitleAbout from "./TitleAbout";
 
 const TeamDetail = () => {
   return (
-    <MainCard padding={7} center gap={10} direction="row">
-      <TeamDetailItem
-        name="Muhammad Nurfajri Rahman"
-        desc="Fresh Graduate Teknik Informatika UIN Syarif Hidayatullah Jakarta. Memiliki Passion dalam Web Development"
-        img="images/yelan.jpg"
-        github="https://github.com/Fyn-x"
-        linkedIn="https://www.linkedin.com/in/mnurfajrirahman/"
-        ig="https://www.instagram.com/m_nfajri_r/"
-      />
-      <TeamDetailItem
-        name="Muhammad Fikri Syahid"
-        desc="Fresh Graduate Teknik Informatika UIN Syarif Hidayatullah Jakarta. Memiliki Passion dalam Web Development"
-        img="https://avatars.githubusercontent.com/u/47740296?v=4"
-        github="https://github.com/fikrisyahid"
-        linkedIn="https://www.linkedin.com/in/muhammad-fikri-syahid/"
-        ig="https://www.instagram.com/fs3120_/"
-      />
-    </MainCard>
+    <>
+      <TitleAbout title="Core Team" />
+      <MainCard padding={7} center gap={10} direction="row">
+        <TeamDetailItem
+          name="Muhammad Nurfajri Rahman"
+          desc="Fresh Graduate Teknik Informatika UIN Syarif Hidayatullah Jakarta. Memiliki Passion dalam Web Development"
+          img="images/yelan.jpg"
+          github="https://github.com/Fyn-x"
+          linkedIn="https://www.linkedin.com/in/mnurfajrirahman/"
+          ig="https://www.instagram.com/m_nfajri_r/"
+        />
+        <TeamDetailItem
+          name="Muhammad Fikri Syahid"
+          desc="Fresh Graduate Teknik Informatika UIN Syarif Hidayatullah Jakarta. Memiliki Passion dalam Web Development"
+          img="https://avatars.githubusercontent.com/u/47740296?v=4"
+          github="https://github.com/fikrisyahid"
+          linkedIn="https://www.linkedin.com/in/muhammad-fikri-syahid/"
+          ig="https://www.instagram.com/fs3120_/"
+        />
+      </MainCard>
+    </>
   );
 };
 
