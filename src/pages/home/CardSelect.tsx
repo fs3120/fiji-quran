0e5d99e4c3f1d9fb3325f68eb9c0944e97018c14
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-19 10:50:49 +0700

    homepage content progress

---
+++ src/pages/home/CardSelect.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import MainCard from "../../components/MainCard";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+interface CardSelectProps {
+  logo: ReactNode;
+  text: string;
+  link: string;
+}
+
+export default function CardSelect({ logo, text, link }: CardSelectProps) {
+  const navigate = useNavigate();
+  return (
+    <Button
+      onClick={() => navigate(link)}
+      sx={{ color: "white" }}
+      disableRipple
+    >
+      <MainCard
+        center
+        padding={4}
+        gap={2}
+        sx={{ backgroundColor: "#68b883", borderRadius: 3, width: '17vw' }}
+      >
+        {logo}
+        <Typography variant="h5" fontWeight="light" color="white">
+          {text}
+        </Typography>
+      </MainCard>
+    </Button>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,36 @@
 import MainCard from "../../components/MainCard";
 import PageWrapper from "../../components/PageWrapper";
 import Typography from "@mui/material/Typography";
+import CardSelect from "./CardSelect";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 
 export default function HomePage() {
   return (
     <PageWrapper title="Homepage">
-      <MainCard>
-        <Typography variant="h4">Homepage</Typography>
+      <MainCard center padding={7} gap={3}>
+        <Typography variant="h2">Selamat datang di Fiji Qur'an</Typography>
+        <Typography variant="h5" fontWeight="light">
+          Baca Al-Qur'an beserta tafsirnya dengan mudah
+        </Typography>
+        <MainCard center direction="row" gap={3}>
+          <CardSelect
+            logo={<BookmarksIcon sx={{ color: "white" }} />}
+            text="Baca Per Juz"
+            link="/juz"
+          />
+          <CardSelect
+            logo={<LibraryBooksIcon sx={{ color: "white" }} />}
+            text="Baca Per Surat"
+            link="/surat"
+          />
+          <CardSelect
+            logo={<MenuBookIcon sx={{ color: "white" }} />}
+            text="Baca Per Halaman"
+            link="/halaman"
+          />
+        </MainCard>
       </MainCard>
     </PageWrapper>
   );
