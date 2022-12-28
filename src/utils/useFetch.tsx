b67ce564b4d274fea44004fb704ad8279700446d
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-12-28 10:16:49 +0700

    remove interface from useEffect hook

--- src/utils/useFetch.tsx
+++ src/utils/useFetch.tsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
 
-interface Data {
-  nama: string;
-}
-
 const useFetch = (url:string) => {
   const [data, setData] = useState<any>([]);
   useEffect(() => {
