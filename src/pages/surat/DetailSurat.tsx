8c924c95d4287471cf0de75f40b67ed75d4cb1fc
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-04 04:50:43 +0700

    progress detail surat page (ayat is not shown yet)

--- src/pages/surat/DetailSurat.tsx
+++ src/pages/surat/DetailSurat.tsx
@@ -1,10 +1,45 @@
+import { useParams } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import useFetch from "../../utils/hook/useFetch";
+import { RAPISuratDetail } from "../../interfaces";
+import MainCard from "../../components/MainCard";
+import StringToHTML from "../../components/StringToHTML";
+import LoadingIndicator from "../../components/LoadingIndicator";
+
 const DetailSurat = () => {
+  const { id } = useParams();
+  const data = useFetch<RAPISuratDetail>({
+    url: `https://equran.id/api/surat/${id}`,
+    log: true,
+  });
   return (
-    <>
-      <h1>
-        ini halaman ayat
-      </h1>
-    </>
+    <MainCard center gap={1} sx={{ paddingLeft: '10vw', paddingRight: '10vw' }}>
+      {data ? (
+        <>
+          <Typography variant="h3" mt={5}>
+            {data?.nama}
+          </Typography>
+          <Typography variant="body1" fontSize={18} fontWeight={300}>
+            {data?.nomor}. ({data?.nama_latin} / {data?.arti})
+          </Typography>
+          <Typography variant="body1" fontSize={18} fontWeight={300}>
+            Diturunkan di {data?.tempat_turun}
+          </Typography>
+          <Typography variant="body1" fontSize={18} fontWeight={300}>
+            Jumlah ayat: {data?.jumlah_ayat}
+          </Typography>
+          <StringToHTML
+            variant="body1"
+            fontSize={16}
+            fontWeight={300}
+            text={data?.deskripsi || ""}
+            sx={{ textAlign: "center" }}
+          />
+        </>
+      ) : (
+        <LoadingIndicator />
+      )}
+    </MainCard>
   );
 };
 
