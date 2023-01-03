0400d50a05675ae249b6f1ca063947cac9981e4f
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-04 04:22:47 +0700

    change loading indicator component's name & remove unnecessary div

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -2,22 +2,20 @@ import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 
-export default function SimpleBackdrop() {
+export default function LoadingIndicator() {
   return (
-    <div>
-      <Backdrop
-        sx={{
-          color: "#fff",
-          display:"flex",
-          flexDirection:"column",
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-          alignContent: "space-around",
-        }}
-        open={true}
-      >
-        <CircularProgress color="inherit" />
-        <Typography mt={2}>Loading...</Typography>
-      </Backdrop>
-    </div>
+    <Backdrop
+      sx={{
+        color: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        alignContent: "space-around",
+      }}
+      open={true}
+    >
+      <CircularProgress color="inherit" />
+      <Typography mt={2}>Loading...</Typography>
+    </Backdrop>
   );
 }
