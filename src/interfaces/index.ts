7ecce45cf63ba38e65bb36e4e91c81b1519f7e24
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-04 03:50:24 +0700

    add interface for detail surat

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,30 @@ export interface RAPISurat {
   deskripsi: string;
   audio: string;
 }
+
+export interface RAPIAyat {
+  id: number;
+  surah: number;
+  nomor: number;
+  ar: string;
+  tr: string;
+  idn: string;
+}
+
+interface SuratDetailBase {
+  nomor: number;
+  nama: string;
+  nama_latin: string;
+  jumlah_ayat: number;
+  tempat_turun: string;
+  arti: string;
+  deskripsi: string;
+  audio: string;
+}
+
+export interface RAPISuratDetail extends SuratDetailBase {
+  status: boolean;
+  ayat: RAPIAyat[];
+  surat_selanjutnya: SuratDetailBase;
+  surat_sebelumnya: boolean;
+}
