09015c9ff95a6fd5d0adac3286230b8cbcefd67b
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-04 04:19:16 +0700

    add component to convert string to html

---
+++ src/components/StringToHTML.tsx
@@ -0,0 +1,42 @@
+import Typography from "@mui/material/Typography";
+
+interface StringToHTMLProps {
+  text: string;
+  variant?:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "subtitle1"
+    | "subtitle2"
+    | "body1"
+    | "body2"
+    | "caption"
+    | "button"
+    | "overline"
+    | "inherit"
+    | undefined;
+  fontSize?: number;
+  fontWeight?: number;
+  sx?: any;
+}
+
+export default function StringToHTML({
+  text,
+  variant = "body1",
+  fontSize,
+  fontWeight,
+  sx,
+}: StringToHTMLProps) {
+  return (
+    <Typography
+      variant={variant}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      sx={sx}
+      dangerouslySetInnerHTML={{ __html: text }}
+    />
+  );
+}
