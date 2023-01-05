9f0e34b6489a238bf14d18d089a78128d47ec9ae
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-05 18:06:51 +0700

    progress detail surat

--- src/pages/surat/DetailSurat.tsx
+++ src/pages/surat/DetailSurat.tsx
@@ -1,18 +1,40 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import useFetch from "../../utils/hook/useFetch";
 import { RAPISuratDetail } from "../../interfaces";
 import MainCard from "../../components/MainCard";
 import LoadingIndicator from "../../components/LoadingIndicator";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const DetailSurat = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const data = useFetch<RAPISuratDetail>({
     url: `https://equran.id/api/surat/${id}`,
     log: true,
   });
   return (
-    <MainCard center gap={1} sx={{ paddingLeft: '10vw', paddingRight: '10vw' }}>
+    <MainCard center gap={1} sx={{ paddingLeft: "10vw", paddingRight: "10vw" }}>
+      <Box display="flex" width="100%" justifyContent="space-around">
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => navigate(`/surat/${Number(id) - 1}`)}
+        >
+          Surat Sebelumnya
+        </Button>
+        <Button variant="contained" onClick={() => navigate("/surat")}>
+          Daftar Surat
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => navigate(`/surat/${Number(id) + 1}`)}
+        >
+          Surat Selanjutnya
+        </Button>
+      </Box>
       {data ? (
         <>
           <Typography variant="h3" mt={5}>
