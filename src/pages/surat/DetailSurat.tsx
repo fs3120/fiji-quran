1b28e70332afa9a4955e52a6a7ba54faf88f9359
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-06 21:17:23 +0700

    tafsir data fetching & data processing done

--- src/pages/surat/DetailSurat.tsx
+++ src/pages/surat/DetailSurat.tsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import useFetch from "../../utils/hook/useFetch";
-import { RAPISuratDetail } from "../../interfaces";
+import { RAPISuratDetail, RAPITafsirDetail } from "../../interfaces";
 import MainCard from "../../components/MainCard";
 import LoadingIndicator from "../../components/LoadingIndicator";
 import Ayat from "./Ayat";
 import SuratHeader from "./SuratHeader";
 import ErrorPage from "../error";
 import PageWrapper from "../../components/PageWrapper";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import { useState } from "react";
 
 const DetailSurat = () => {
   const { id } = useParams();
@@ -16,50 +19,80 @@ const DetailSurat = () => {
     return <ErrorPage />;
   }
 
-  const { data } = useFetch<RAPISuratDetail>({
+  const [tafsirContent, setTafsirContent] = useState("");
+  const [tafsirOpen, setTafsirOpen] = useState(false);
+
+  const { data: dataSurat } = useFetch<RAPISuratDetail>({
     url: `https://equran.id/api/surat/${id}`,
+  });
+
+  const { data: dataTafsir } = useFetch<RAPITafsirDetail>({
+    url: `https://equran.id/api/tafsir/${id}`,
     log: true,
   });
 
+  if (!dataSurat && !dataTafsir) {
+    return <LoadingIndicator />;
+  }
+
+  const data = dataSurat?.ayat.map(
+    (ayat) =>
+      dataTafsir?.tafsir
+        .map((tafsir) => {
+          if (tafsir.ayat === ayat.nomor) {
+            return {
+              arab: ayat.ar,
+              nomor: `${ayat.nomor}`,
+              arti: ayat.idn,
+              tafsir: tafsir.tafsir,
+            };
+          }
+          return null;
+        })
+        .filter((e) => e !== null)[0]
+  );
+
   return (
     <PageWrapper title="Detail Surat">
+      <Dialog open={tafsirOpen} onClose={() => setTafsirOpen(false)}>
+        <DialogContent>
+          <Typography variant="body1">{tafsirContent}</Typography>
+        </DialogContent>
+      </Dialog>
       <MainCard
         center
         gap={1}
         sx={{ paddingLeft: "10vw", paddingRight: "10vw" }}
       >
         <SuratHeader id={id} />
-        {data ? (
-          <>
-            <Typography variant="h3" mt={5}>
-              {data?.nama}
-            </Typography>
-            <Typography variant="body1">
-              {data?.nomor}. ({data?.nama_latin} / {data?.arti})
-            </Typography>
-            <Typography variant="body1">
-              Diturunkan di {data?.tempat_turun}
-            </Typography>
-            <Typography variant="body1">
-              Jumlah ayat: {data?.jumlah_ayat}
-            </Typography>
-            <Typography
-              variant="body1"
-              textAlign="center"
-              dangerouslySetInnerHTML={{ __html: data?.deskripsi }}
-            />
-            {data?.ayat.map((ayat, index) => (
-              <Ayat
-                arab={ayat.ar}
-                nomor={`${ayat.nomor}`}
-                arti={ayat.idn}
-                key={index}
-              />
-            ))}
-          </>
-        ) : (
-          <LoadingIndicator />
-        )}
+        <Typography variant="h3" mt={5}>
+          {dataSurat?.nama}
+        </Typography>
+        <Typography variant="body1">
+          {dataSurat?.nomor}. ({dataSurat?.nama_latin} / {dataSurat?.arti})
+        </Typography>
+        <Typography variant="body1">
+          Diturunkan di {dataSurat?.tempat_turun}
+        </Typography>
+        <Typography variant="body1">
+          Jumlah ayat: {dataSurat?.jumlah_ayat}
+        </Typography>
+        <Typography
+          variant="body1"
+          textAlign="center"
+          dangerouslySetInnerHTML={{ __html: dataSurat?.deskripsi || "" }}
+        />
+        {data?.map((data, index) => (
+          <Ayat
+            setTafsirContent={setTafsirContent}
+            setTafsirOpen={setTafsirOpen}
+            arab={data?.arab || ""}
+            nomor={`${data?.nomor}`}
+            arti={data?.arti || ""}
+            key={index}
+            tafsir={data?.tafsir || ""}
+          />
+        ))}
       </MainCard>
     </PageWrapper>
   );
