4b47946dd322ddc8d799569ad979b58422d02f26
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-09 14:45:49 +0700

    prettier

--- src/pages/surat/index.tsx
+++ src/pages/surat/index.tsx
@@ -1,12 +1,18 @@
 import PageWrapper from "../../components/PageWrapper";
 import Typography from "@mui/material/Typography";
 import ListSurat from "./ListSurat";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
 
 export default function SuratPage() {
   return (
     <PageWrapper title="Daftar Surat">
-      <Typography color={"#68B984"} fontWeight="bold" variant="h4" mt={10} textAlign={"center"}>
+      <Typography
+        color={"#68B984"}
+        fontWeight="bold"
+        variant="h4"
+        mt={10}
+        textAlign={"center"}
+      >
         Daftar Surat
       </Typography>
       <Divider variant="middle" />
