da347525d392e783cbf483aa555fd51fed323697
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-09 15:35:09 +0700

    add list juz

---
+++ src/pages/juz/ListJuz.tsx
@@ -0,0 +1,33 @@
+import MainCard from "../../components/MainCard";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import generateJuz from "../../utils/generate-juz";
+
+const ListJuz = () => {
+  const navigate = useNavigate();
+  const listJuz = generateJuz();
+  return (
+    <MainCard direction="row" wrap center gap={2} padding={5}>
+      {listJuz.map((juz) => (
+        <Button
+          key={juz}
+          onClick={() => navigate(`${juz}`)}
+          variant="contained"
+          sx={{
+            backgroundColor: "#68B984",
+            width: "20em",
+            height: "5em",
+            ":hover": { backgroundColor: "#3e8055" },
+          }}
+        >
+          <Typography fontSize="1.5em" fontFamily="Calibri">
+            {`Juz ${juz}`}
+          </Typography>
+        </Button>
+      ))}
+    </MainCard>
+  );
+};
+
+export default ListJuz;
--- src/pages/juz/index.tsx
+++ src/pages/juz/index.tsx
@@ -1,13 +1,23 @@
 import MainCard from "../../components/MainCard";
 import PageWrapper from "../../components/PageWrapper";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListJuz from "./ListJuz";
 
 export default function JuzPage() {
   return (
     <PageWrapper title="Juz Page">
-      <MainCard>
-        <Typography variant="h4">Ini halaman juz</Typography>
-      </MainCard>
+      <Typography
+        color={"#68B984"}
+        fontWeight="bold"
+        variant="h4"
+        mt={10}
+        textAlign={"center"}
+      >
+        Daftar Juz
+      </Typography>
+      <Divider variant="middle" />
+      <ListJuz />
     </PageWrapper>
   );
 }
