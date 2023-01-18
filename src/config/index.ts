ef7e94b189bfdcb8bea596318d71203bd261c746
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-18 17:08:59 +0700

    fix PAGE URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const SURAH_URL = `${API_URL}/surah`;
 const JUZ_URL = `${API_URL}/juz`;
-const PAGE_URL = `${API_URL}/halaman`;
+const PAGE_URL = `${API_URL}/page`;
 
 export { API_URL, SURAH_URL, JUZ_URL, PAGE_URL };
