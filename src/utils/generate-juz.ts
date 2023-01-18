3902fc6bef21aa5a6ac9f6afd7fef231d1775cbf
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2023-01-18 17:00:26 +0700

    update generate juz to generate number

--- src/utils/generate-juz.ts
+++
@@ -1,7 +0,0 @@
-export default function generateJuz() {
-  const result = [];
-  for (let i = 1; i <= 30; i++) {
-    result.push(i);
-  }
-  return result;
-}
---
+++ src/utils/generate-number.ts
@@ -0,0 +1,7 @@
+export default function generateNumber(start: number, end: number) {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
